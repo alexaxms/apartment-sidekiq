bc2bac7f7a088cdec29529a492bd823695d14475
alexaxms <alexaxmss@hotmail.com>
2019-11-25 15:28:56 -0300

    class names fix

--- lib/apartment/sidekiq/railtie.rb
+++ lib/apartment/sidekiq/railtie.rb
@@ -1,7 +1,7 @@
 module Apartment::Sidekiq
   class Railtie < Rails::Railtie
     initializer "apartment.sidekiq" do
-      Apartment::Sidekiq::Middleware.run
+      ::Apartment::Sidekiq::Middleware.run
     end
   end
 end
